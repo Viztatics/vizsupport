5a98e836d11b11baaf1eed53ecee5561f87f8c73
yingxin <liyx8610@gmail.com>
2018-06-18 14:24:39 -0400

    add web pages

--- app/views.py
+++ app/views.py
@@ -1,6 +1,6 @@
 from flask import render_template
 from flask_appbuilder.models.sqla.interface import SQLAInterface
-from flask_appbuilder import ModelView
+from flask_appbuilder import AppBuilder, BaseView, ModelView, expose, has_access
 from app import appbuilder, db
 
 """
@@ -20,10 +20,26 @@ from app import appbuilder, db
 """
     Application wide 404 error handler
 """
+class RuleView(BaseView):
+    
+    route_base = '/rules'
+    default_view = 'highRiskCountry'
+
+    @expose('/highRiskCountry/')
+    @has_access
+    def highRiskCountry(self):
+        # do something with param1
+        # and return it
+        return self.render_template('rule_high_risk_country.html')
+
+
+
 @appbuilder.app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html', base_template=appbuilder.base_template, appbuilder=appbuilder), 404
 
 db.create_all()
+
+appbuilder.add_view(RuleView, "High Risk Countries", category='Rules')
 
 
