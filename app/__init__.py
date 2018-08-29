e69f1be38e3fb3869dccbac482095870c368b129
yingxin <liyx8610@gmail.com>
2018-08-29 12:38:37 -0400

    edit welcome page change welcome to company name

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,7 @@
 import logging
 from flask import Flask
 from flask_appbuilder import SQLA, AppBuilder
-from .viztatics import VizSecurityManager
+from .viztatics import VizSecurityManager,VizIndexView
 
 """
  Logging configuration
@@ -13,7 +13,7 @@ logging.getLogger().setLevel(logging.DEBUG)
 app = Flask(__name__)
 app.config.from_object('config')
 db = SQLA(app)
-appbuilder = AppBuilder(app, db.session, security_manager_class=VizSecurityManager)
+appbuilder = AppBuilder(app, db.session, security_manager_class=VizSecurityManager, indexview=VizIndexView)
 
 
 """
